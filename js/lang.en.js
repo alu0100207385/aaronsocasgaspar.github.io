b73373410029e4e4c71bfe51354064622544ff10
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2017-03-18 19:21:03 +0000

    added changes

---
+++ js/lang.en.js
@@ -0,0 +1,19 @@
+lang = {
+    "rol" : "Developer",
+    "resume1" : "My name is Aarón Socas Gaspar. Graduated in computer engineering in La Laguna University (Tenerife - Spain). I have focused my training in information technology, which contais knowegle related with web and smartphone developer, segurity and information processing.",
+    "resume2" : "I consider myself a working person, active and dinamic. It is important for me, the target of the project, this is useful to effort more in my work. I want to go on learning and colaborate en interesting projects.",
+    "location" : "S/C de Tenerife. Spain",
+    "email" : "Email",
+    "linkedin" : "See my Linkedin",
+    "github" : "Access to my repository",
+    "car" : "Own vehicle",
+    "coverLetterTitle" : "COVER LETTER",
+    "coverLetter" : "Estimado Sr/Sra:"
+                +"Mi nombre es Aarón Socas Gaspar, soy Graduado en Ingeniería Informática por la ULL. Decidí formarme en el ámbito tecnológico porque siempre me llamó la atención así como sus aplicaciones y salida laboral."
+                +"Actualmente estoy trabajando para Atos dentro del departamento de I+D+i, investigando y desarrollando módulos relacionados con la seguridad y privacidad."
+                +"Mi experiencia profesional como desarrollador la he realizado en la empresa INTEGRA Tecnología y Comunicación de Canarias, SL. También poseo conocimientos en administración de sistemas, tengo el título de formación profesional 2º grado de Diplomado Técnico Superior en Sistemas y estuve como becario en la Cámara de Comercio de Santa Cruz de Tenerife y en la Universidad de La Laguna. He orientado mi formación hacia la tecnología web, desarrollo en la nube, gestión de contenidos y seguridad."
+                +"Soy una persona activa, curiosa. responsable, constante y perfeccionista, con una gran capacidad de trabajo y esfuerzo. Soy capaz de adaptarme a las distintas situaciones y a los cambios, pudiendo trabajar de forma efectiva en equipo o individual."
+                +"Un cordial saludo,"
+                +"Aarón Socas Gaspar"
+};
+
