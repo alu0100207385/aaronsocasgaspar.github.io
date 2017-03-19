96b369f52e341f9ef8eb8bf2e55b0ee85f73f449
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2017-03-19 17:23:18 +0000

    more translations + style improvements

--- js/lang.en.js
+++ js/lang.en.js
@@ -58,7 +58,127 @@ langEn = {
   "educationCesarManriqueTime" : "2005/09 - 2007/07",
   "educationCesarManriqueRol" : "Computer Systems Technician",
   "educationCesarManriqueCompany" : "IES Cesar Manrique",
-  "educationCesarManriqueDetails" : "<p>Deploy and manage computer systems, network management, software installation and changes to improve system and application exploitation.</p>"
+  "educationCesarManriqueDetails" : "<p>Deploy and manage computer systems, network management, software installation and changes to improve system and application exploitation.</p>",
+  "googleActivateDetails" : "A platform born with the aim of helping young people to have easier access not only to knowledge but also to training, entrepreneurship and the professional world.",
+  "googleActivateMobileCourse" : "Mobile Apps Development course",
+  "googleActivateMobileEmisor" : "Complutense University of Madrid",
+  "googleActivateMobileDetails" : "Essential principles for the creation, design and programming of mobile applications.",
+  "googleActivateWebCourse" : "Introduction to Web Development course (1)",
+  "googleActivateWebEmisor" : "Institute of International Economics",
+  "googleActivateWebDetails" : "Introduction to web design. History of the Web and creation of correct web pages with HTML5.",
+  "googleActivateCloudCourse" : "Cloud Computing course",
+  "googleActivateCloudEmisor" : "School of Industrial Organization",
+  "googleActivateCloudDetails" : "Possibilities offered by the Cloud world to business. How to innovate to adapt to new technologies. Access to information from any device and securely.",
+  "iterCourse" : "Center for Excellence in Development and Innovation (CEDEeI)",
+  "iterCompany" : "Technological and Renewable Energy Institute",
+  "iterDetails" : "At present, ITER is an international research center of reference in renewable energy, engineering, telecommunications and the environment. Here I learned different technologies, Java and Android among others.",
+  "codeSchoolDetails" : "Code School is an online learning destination for existing and aspiring developers that teaches through entertaining content. Each course is built around a creative theme and storyline so that it feels like you’re playing a game, not sitting in a classroom. We combine gaming mechanics with video instruction and in-browser coding challenges to make learning fun and memorable. With over 60 courses covering HTML/CSS, JavaScript, Ruby, Python, .NET, iOS, Git, databases, and more, Code School pairs experienced instructors with meticulously produced, high-quality content inspired by our community and network of members.",
+  "rubyCourseDetails" : "Elements of basic building blocks in Ruby.",
+  "angularCourseDetails" : "Angular allows your application to have an expanded HTML library. Learn to add to your HTML behavior and accelerate the responsiveness of the application.",
+  "javascriptCourse1Details" : "An introduction to the basics of the JavaScript language. Build a base of JavaScript syntax and learn how to use values, variables and files.",
+  "gitCourseDetails" : "Learn how to use Git by reviewing the basics of Git version control. Introductory course that was created with GitHub.",
+  "sqlCourseDetails" : "Basic skills of manipulation in database used in the SQL programming language.",
+  "jqueryCourseDetails" : "JQuery 2.0 basics, a JavaScript library, modify page elements and improve user interaction.",
+  "portfolioTime" : "2017/03/19*",
+  "portfolioLastUpdate" : "*Last update",
+  "portfolioView" : "See portfolio",
+  "portfolioDetails" : "Here you can find some of my projects, practices and works in which I have collaborated.",
+  "getCv" : "Get CV"
 
 };
+
+
+$(document).ready(function() {
+  $('#translateToEnglish').click(function(){
+    translateToEnglish();
+  });
+});
+
+function translateToEnglish(){
+  $('#dropdownNav').text(langEn["dropdownNav"]);
+  $('#professionalExperienceNav').text(langEn["professionalExperienceNav"]);
+  $('#educationNav').text(langEn["educationNav"]);
+  $('#certificationsNav').text(langEn["certificationsNav"]);
+  $('#portfolioNav').text(langEn["portfolioNav"]);
+  $('#professionalExperience').text(langEn["professionalExperience"]);
+  $('#education').text(langEn["education"]);
+  $('#certifications').text(langEn["certifications"]);
+  $('#portfolio').text(langEn["portfolio"]);
+  $('#rol').text(langEn["rol"]);
+  $('#resume').html(langEn["resume"]);
+  $('#coverLetterTitle').text(langEn["coverLetterTitle"]);
+  $('#coverLetter').html(langEn["coverLetter"]);
+  $('#printLetter').text(langEn["printLetter"]);
+  $('#atosTime').text(langEn["atosTime"]);
+  $('#atosRol').text(langEn["atosRol"]);
+  $('#atosTasks').html(langEn["atosTasks"]);
+  $('#integraTime').text(langEn["integraTime"]);
+  $('#integraRol').text(langEn["integraRol"]);
+  $('#integraTasks').html(langEn["integraTasks"]);
+  $('#laLagunaTime').text(langEn["laLagunaTime"]);
+  $('#laLagunaRol').text(langEn["laLagunaRol"]);
+  $('#laLagunaCompany').text(langEs["laLagunaCompany"]);
+  $('#laLagunaTasks').html(langEn["laLagunaTasks"]);
+  $('#chamberOfCommerceTime').text(langEn["chamberOfCommerceTime"]);
+  $('#chamberOfCommerceRol').text(langEn["chamberOfCommerceRol"]);
+  $('#chamberOfCommerceCompany').text(langEn["chamberOfCommerceCompany"]);
+  $('#chamberOfCommerceTasks').html(langEn["chamberOfCommerceTasks"]);
+  $('#educationLaLagunaTime').text(langEn["educationLaLagunaTime"]);
+  $('#educationLaLagunaCareer').text(langEn["educationLaLagunaCareer"]);
+  $('#educationLaLagunaCompany').text(langEn["laLagunaCompany"]);
+  $('#educationLaLagunaDetails').html(langEn["educationLaLagunaDetails"]);
+  $('#educationCesarManriqueTime').text(langEn["educationCesarManriqueTime"]);
+  $('#educationCesarManriqueRol').text(langEn["educationCesarManriqueRol"]);
+  $('#educationCesarManriqueCompany').text(langEn["educationCesarManriqueCompany"]);
+  $('#educationCesarManriqueDetails').html(langEn["educationCesarManriqueDetails"]);
+  $('#googleActivateDetails').text(langEn["googleActivateDetails"]);
+  $('#googleActivateMobileCourse').text(langEn["googleActivateMobileCourse"]);
+  $('#googleActivateMobileEmisor').text(langEn["googleActivateMobileEmisor"]);
+  $('#googleActivateMobileDetails').text(langEn["googleActivateMobileDetails"]);
+  $('#googleActivateWebCourse').text(langEn["googleActivateWebCourse"]);
+  $('#googleActivateWebEmisor').text(langEn["googleActivateWebEmisor"]);
+  $('#googleActivateWebDetails').text(langEn["googleActivateWebDetails"]);
+  $('#googleActivateCloudCourse').text(langEn["googleActivateCloudCourse"]);
+  $('#googleActivateCloudEmisor').text(langEn["googleActivateCloudEmisor"]);
+  $('#googleActivateCloudDetails').text(langEn["googleActivateCloudDetails"]);
+  $('#iterCourse').text(langEn["iterCourse"]);
+  $('#iterCompany').text(langEn["iterCompany"]);
+  $('#iterDetails').text(langEn["iterDetails"]);
+  $('#codeSchoolDetails').text(langEn["codeSchoolDetails"]);
+  $('#rubyCourseDetails').text(langEn["rubyCourseDetails"]);
+  $('#angularCourseDetails').text(langEn["angularCourseDetails"]);
+  $('#javascriptCourse1Details').text(langEn["javascriptCourse1Details"]);
+  $('#gitCourseDetails').text(langEn["gitCourseDetails"]);
+  $('#sqlCourseDetails').text(langEn["sqlCourseDetails"]);
+  $('#jqueryCourseDetails').text(langEn["jqueryCourseDetails"]);
+  $('#portfolioTime').text(langEn["portfolioTime"]);
+  $('#portfolioLastUpdate').text(langEn["portfolioLastUpdate"]);
+  $('#portfolioView').text(langEn["portfolioView"]);
+  $('#portfolioDetails').text(langEn["portfolioDetails"]);
+  $('#getCv').text(langEn["getCv"]);
+
+
+  $('#location').text(langEn["location"]);
+  $('#email').text(langEn["email"]);
+  $('#linkedin').text(langEn["linkedin"]);
+  $('#github').text(langEn["github"]);
+  $('#car').text(langEn["car"]);
+  $('#proffesionalSkills').text(langEn["proffesionalSkills"]);
+  $('#languages').text(langEn["languages"]);
+  $('#english').text(langEn["english"]);
+  $('#spanish').text(langEn["spanish"]);
+  $('#otherKnowledge').text(langEn["otherKnowledge"]);
+  $('#repositoryDevelopment').text(langEn["repositoryDevelopment"]);
+  $('#testMethods').text(langEn["testMethods"]);
+  $('#testTools').text(langEn["testTools"]);
+  $('#manageTools').text(langEn["manageTools"]);
+  $('#agileMthodology').text(langEn["agileMthodology"]);
+  $('#clouds').text(langEn["clouds"]);
+  $('#database').text(langEn["database"]);
+  $('#os').text(langEn["os"]);
+
+  // Change active class
+  $('#linkEng').addClass('active');
+  $('#linkEsp').removeClass('active');
+  }
 
--- js/lang.es.js
+++ js/lang.es.js
@@ -62,6 +62,121 @@ langEs = {
   "educationCesarManriqueTime" : "09/2005 - 07/2007",
   "educationCesarManriqueRol" : "Técnico superior de sistemas informáticos",
   "educationCesarManriqueCompany" : "IES Cesar Manrique",
-  "educationCesarManriqueDetails" : "<p>Implantar y administrar sistemas informáticos, gestión de redes, instalación de software y cambios para mejorar la explotación del sistema y las aplicaciones</p>."
-
+  "educationCesarManriqueDetails" : "<p>Implantar y administrar sistemas informáticos, gestión de redes, instalación de software y cambios para mejorar la explotación del sistema y las aplicaciones</p>.",
+  "googleActivateDetails" : "Una plataforma que nace con el objetivo de ayudar a los jóvenes a que tengan un más fácil acceso no solo al conocimiento sino también a la formación, al emprendimiento y al mundo profesional.",
+  "googleActivateMobileCourse" : "Curso de Desarrollo de Apps Móviles",
+  "googleActivateMobileEmisor" : "Universidad Complutense de Madrid",
+  "googleActivateMobileDetails" : "Principios esenciales para la creación, diseño y programación de aplicaciones móviles.",
+  "googleActivateWebCourse" : "Curso de Introducción al Desarrollo Web (1)",
+  "googleActivateWebEmisor" : "Instituto de Economía Internacional",
+  "googleActivateWebDetails" : "Introducción al diseño web. Historia de la Web y creación de páginas web correctas con HTML5.",
+  "googleActivateCloudCourse" : "Curso de Cloud Computing",
+  "googleActivateCloudEmisor" : "Escuela de Organización Industrial",
+  "googleActivateCloudDetails" : "Posibilidades que ofrece el mundo Cloud a los negocios. Cómo innovar para adaptarse las nuevas tecnologías. Acceso a la información desde cualquier dispositivo y de forma segura.",
+  "iterCourse" : "Centro de Excelencia de Desarrollo e Innovación (CEDEeI)",
+  "iterCompany" : "Instituto Tecnológico y de Energías Renovables",
+  "iterDetails" : "En la actualidad, el ITER es un centro de investigación de referencia internacional en energías renovables, ingeniería, telecomunicaciones y medio ambiente. Aquí aprendí distintas tecnologías, Java y Android entre otras.",
+  "codeSchoolDetails" : "Code School es un portal online dirigido a desarrollaodres que enseña a través de contenidos de entretenimiento. Con más de 50 cursos (Ruby, JavaScript, HTML/CSS, iOS, Git, bases de datos, etc.) Code School junta experimentados instructores con un contenido atractivo y de alta calidad inspirado para su comunidad. Más de 1 millón de personas en todo el mundo visita Code School para mejorar sus habilidades de desarrollo y aprender sobre la marcha.",
+  "rubyCourseDetails" : "Elementos de bloques de construcción básicos en Ruby.",
+  "angularCourseDetails" : "Angular permite que su aplicación tenga una biblioteca de HTML ampliado. Aprender a sumar a su comportamiento HTML y acelerar la capacidad de respuesta de la aplicación.",
+  "javascriptCourse1Details" : "Una introducción a los conceptos básicos del lenguaje JavaScript. Construir una base de la sintaxis de JavaScript y aprender a utilizar los valores, variables y archivos.",
+  "gitCourseDetails" : "Aprende a usar Git mediante la revisión de los conceptos básicos de control de versiones Git. Curso de introducción que se creó con GitHub.",
+  "sqlCourseDetails" : "Lecciones de habilidades básicas de manipulación en base de datos utilizado en el lenguaje de programación SQL.",
+  "jqueryCourseDetails" : "Conceptos básicos de jQuery 2.0, una biblioteca JavaScript, modificar elementos de la página y mejorar la interacción del usuario.",
+  "portfolioTime" : "19/03/2017*",
+  "portfolioLastUpdate" : "*Última actualización",
+  "portfolioView" : "Ver portafolio",
+  "portfolioDetails" : "Aquí podrás encontrar algunos de mis proyectos, prácticas y trabajos en los que he colaborado.",
+  "getCv" : "Obtener CV"
 };
+
+$(document).ready(function() {
+
+  $('#translateToSpanish').click(function(){
+    $('#dropdownNav').text(langEs["dropdownNav"]);
+    $('#professionalExperienceNav').text(langEs["professionalExperienceNav"]);
+    $('#educationNav').text(langEs["educationNav"]);
+    $('#certificationsNav').text(langEs["certificationsNav"]);
+    $('#portfolioNav').text(langEs["portfolioNav"]);
+    $('#professionalExperience').text(langEs["professionalExperience"]);
+    $('#education').text(langEs["education"]);
+    $('#certifications').text(langEs["certifications"]);
+    $('#portfolio').text(langEs["portfolio"]);
+    $('#rol').text(langEs["rol"]);
+    $('#resume').html(langEs["resume"]);
+    $('#coverLetterTitle').text(langEs["coverLetterTitle"]);
+    $('#coverLetter').html(langEs["coverLetter"]);
+    $('#printLetter').text(langEs["printLetter"]);
+    $('#atosTime').text(langEs["atosTime"]);
+    $('#atosRol').text(langEs["atosRol"]);
+    $('#atosTasks').html(langEs["atosTasks"]);
+    $('#integraTime').text(langEs["integraTime"]);
+    $('#integraRol').text(langEs["integraRol"]);
+    $('#integraTasks').html(langEs["integraTasks"]);
+    $('#laLagunaTime').text(langEs["laLagunaTime"]);
+    $('#laLagunaRol').text(langEs["laLagunaRol"]);
+    $('#laLagunaCompany').text(langEs["laLagunaCompany"]);
+    $('#laLagunaTasks').html(langEs["laLagunaTasks"]);
+    $('#chamberOfCommerceTime').text(langEs["chamberOfCommerceTime"]);
+    $('#chamberOfCommerceRol').text(langEs["chamberOfCommerceRol"]);
+    $('#chamberOfCommerceCompany').text(langEs["chamberOfCommerceCompany"]);
+    $('#chamberOfCommerceTasks').html(langEs["chamberOfCommerceTasks"]);
+    $('#educationLaLagunaTime').text(langEs["educationLaLagunaTime"]);
+    $('#educationLaLagunaCareer').text(langEs["educationLaLagunaCareer"]);
+    $('#educationLaLagunaCompany').text(langEs["laLagunaCompany"]);
+    $('#educationLaLagunaDetails').html(langEs["educationLaLagunaDetails"]);
+    $('#educationCesarManriqueTime').text(langEs["educationCesarManriqueTime"]);
+    $('#educationCesarManriqueRol').text(langEs["educationCesarManriqueRol"]);
+    $('#educationCesarManriqueCompany').text(langEs["educationCesarManriqueCompany"]);
+    $('#educationCesarManriqueDetails').html(langEs["educationCesarManriqueDetails"]);
+    $('#googleActivateDetails').text(langEs["googleActivateDetails"]);
+    $('#googleActivateMobileCourse').text(langEs["googleActivateMobileCourse"]);
+    $('#googleActivateMobileEmisor').text(langEs["googleActivateMobileEmisor"]);
+    $('#googleActivateMobileDetails').text(langEs["googleActivateMobileDetails"]);
+    $('#googleActivateWebCourse').text(langEs["googleActivateWebCourse"]);
+    $('#googleActivateWebEmisor').text(langEs["googleActivateWebEmisor"]);
+    $('#googleActivateWebDetails').text(langEs["googleActivateWebDetails"]);
+    $('#googleActivateCloudCourse').text(langEs["googleActivateCloudCourse"]);
+    $('#googleActivateCloudEmisor').text(langEs["googleActivateCloudEmisor"]);
+    $('#googleActivateCloudDetails').text(langEs["googleActivateCloudDetails"]);
+    $('#iterCourse').text(langEs["iterCourse"]);
+    $('#iterCompany').text(langEs["iterCompany"]);
+    $('#iterDetails').text(langEs["iterDetails"]);
+    $('#codeSchoolDetails').text(langEs["codeSchoolDetails"]);
+    $('#rubyCourseDetails').text(langEs["rubyCourseDetails"]);
+    $('#angularCourseDetails').text(langEs["angularCourseDetails"]);
+    $('#javascriptCourse1Details').text(langEs["javascriptCourse1Details"]);
+    $('#gitCourseDetails').text(langEs["gitCourseDetails"]);
+    $('#sqlCourseDetails').text(langEs["sqlCourseDetails"]);
+    $('#jqueryCourseDetails').text(langEs["jqueryCourseDetails"]);
+    $('#portfolioTime').text(langEs["portfolioTime"]);
+    $('#portfolioLastUpdate').text(langEs["portfolioLastUpdate"]);
+    $('#portfolioView').text(langEs["portfolioView"]);
+    $('#portfolioDetails').text(langEs["portfolioDetails"]);
+    $('#getCv').text(langEs["getCv"]);
+
+
+    $('#location').text(langEs["location"]);
+    $('#email').text(langEs["email"]);
+    $('#linkedin').text(langEs["linkedin"]);
+    $('#github').text(langEs["github"]);
+    $('#car').text(langEs["car"]);
+    $('#proffesionalSkills').text(langEs["proffesionalSkills"]);
+    $('#languages').text(langEs["languages"]);
+    $('#english').text(langEs["english"]);
+    $('#spanish').text(langEs["spanish"]);
+    $('#otherKnowledge').text(langEs["otherKnowledge"]);
+    $('#repositoryDevelopment').text(langEs["repositoryDevelopment"]);
+    $('#testMethods').text(langEs["testMethods"]);
+    $('#testTools').text(langEs["testTools"]);
+    $('#manageTools').text(langEs["manageTools"]);
+    $('#agileMthodology').text(langEs["agileMthodology"]);
+    $('#clouds').text(langEs["clouds"]);
+    $('#database').text(langEs["database"]);
+    $('#os').text(langEs["os"]);
+
+    // Change active class
+    $('#linkEng').removeClass('active');
+    $('#linkEsp').addClass('active');
+  });
+});
