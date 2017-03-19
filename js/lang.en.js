a497b5c35666b01ffe9c14d7fb47d76eba180c8a
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2017-03-19 11:40:27 +0000

    improvements

--- js/lang.en.js
+++ js/lang.en.js
@@ -1,32 +1,41 @@
 langEn = {
-    "rol" : "Developer",
-    "resume1" : "My name is Aarón Socas Gaspar. Graduated in computer engineering in La Laguna University (Tenerife - Spain). I have focused my training in information technology, which contais knowegle related with web and smartphone developer, segurity and information processing.",
-    "resume2" : "I consider myself as a hard worker person, active and dinamic. It is important for me, the target of the project, this is useful to effort more in my work. I want to go on learning and colaborate en interesting projects.",
-    "location" : "S/C de Tenerife. Spain",
-    "email" : "Email",
-    "linkedin" : "See my Linkedin",
-    "github" : "Access to my repository",
-    "car" : "Own vehicle",
-    "proffesionalSkills" : "PROFFESIONAL SKILLS",
-    "languages" : "LANGUAGES",
-    "english" : "English",
-    "spanish" : "Spanish",
-    "otherKnowledge" : "OTHER KNOWLEDGE",
-    "repositoryDevelopment" : "Collaborative development: Github y Bitbucket",
-    "testMethods" : "Test methods: TDD and continuos integration",
-    "testTools" : "Test tools: Selenium, Coveralls",
-    "manageTools" : "Project manage: PivotalTracker, OpenProject",
-    "agileMthodology" : "Agile methodology (Scrum)",
-    "clouds" : "Clouds: PaaS (Heroku, OpenShift)",
-    "database" : "SQL",
-    "os" : "Linux and Windows",
-    "coverLetterTitle" : "COVER LETTER",
-    "coverLetter" : "Dear Mr/Mrs:"
-                +"My name is Aarón Socas Gaspar, Graduated in computer engineering in La Laguna University (Tenerife - Spain). I decided to train me in technological field because it was always interesting for me as well as its applications and jobs."
-                +"I am working currecly for Atos in I+D+i (investigation, development and innovation) department, studying and developing modules related with security and privacy."
-                +"My profesional experience as developer have performed in INTEGRA Tecnología y Comunicación de Canarias Company. I also have  knowledge in system manage, I have a title job training in 2nd grade of Technical Higher Diploma in Systems and I was working in Chamber of Commerce in Santa Cruz de Tenerife as intern and La Laguna University. I have oriented my training towards web technology, cloud development, content manager and security."
-                +"I am an active persion, curious, responsible, constant and perfectionist, with a great capacity of work and effort. I can adapt to different situations and changes and I can work in team or individually."
-                +"Kindest regards,"
-                +"Aarón Socas Gaspar"
+  "dropdownNav" : "Sections",
+  "professionalExperienceNav" : "Professional experience",
+  "educationNav" : "Education",
+  "certificationsNav" : "Certificaciones",
+  "portfolioNav" : "Portfolio",
+  "professionalExperience" : "PROFESSIONAL EXPERIENCE",
+  "education" : "EDUCATION",
+  "certifications" : "CERTIFICATIONS",
+  "portfolio" : "PORTFOLIO",
+  "rol" : "Developer",
+  "resume1" : "My name is Aarón Socas Gaspar. Graduated in computer engineering in La Laguna University (Tenerife - Spain). I have focused my training in information technology, which contais knowegle related with web and smartphone developer, segurity and information processing.",
+  "resume2" : "I consider myself as a hard worker person, active and dinamic. It is important for me, the target of the project, this is useful to effort more in my work. I want to go on learning and colaborate en interesting projects.",
+  "location" : "S/C de Tenerife. Spain",
+  "email" : "Email",
+  "linkedin" : "See my Linkedin",
+  "github" : "Access to my repository",
+  "car" : "Own vehicle",
+  "proffesionalSkills" : "PROFFESIONAL SKILLS",
+  "languages" : "LANGUAGES",
+  "english" : "English",
+  "spanish" : "Spanish",
+  "otherKnowledge" : "OTHER KNOWLEDGE",
+  "repositoryDevelopment" : "Collaborative development: Github y Bitbucket",
+  "testMethods" : "Test methods: TDD and continuos integration",
+  "testTools" : "Test tools: Selenium, Coveralls",
+  "manageTools" : "Project manage: PivotalTracker, OpenProject",
+  "agileMthodology" : "Agile methodology (Scrum)",
+  "clouds" : "Clouds: PaaS (Heroku, OpenShift)",
+  "database" : "SQL",
+  "os" : "Linux and Windows",
+  "coverLetterTitle" : "COVER LETTER",
+  "coverLetter" : "Dear Mr/Mrs:"
+              +"My name is Aarón Socas Gaspar, Graduated in computer engineering in La Laguna University (Tenerife - Spain). I decided to train me in technological field because it was always interesting for me as well as its applications and jobs."
+              +"I am working currecly for Atos in I+D+i (investigation, development and innovation) department, studying and developing modules related with security and privacy."
+              +"My profesional experience as developer have performed in INTEGRA Tecnología y Comunicación de Canarias Company. I also have  knowledge in system manage, I have a title job training in 2nd grade of Technical Higher Diploma in Systems and I was working in Chamber of Commerce in Santa Cruz de Tenerife as intern and La Laguna University. I have oriented my training towards web technology, cloud development, content manager and security."
+              +"I am an active persion, curious, responsible, constant and perfectionist, with a great capacity of work and effort. I can adapt to different situations and changes and I can work in team or individually."
+              +"Kindest regards,"
+              +"Aarón Socas Gaspar"
 };
 
--- js/lang.es.js
+++ js/lang.es.js
@@ -1,35 +1,44 @@
 langEs = {
-    "rol" : "Desarrollador",
-    "resume1": "Mi nombre es Aarón Socas Gaspar. Graduado en Ingeniería"
-              +"Informática por la Universidad de La Laguna. He orientado mi"
-              +"formación en el uso de tecnologías de la información, la cual"
-              +"engloba conocimientos relacionados con programación web y"
-              +"dispositivos móviles, seguridad y tratamiento de la información.",
-    "resume2" : "Me considero una persona trabajadora, activa y dinámica. Doy importancia al objetivo del proyecto y esto me hace esforzarme más en mi trabajo. Quiero seguir aprendiendo y colaborar en proyectos interesantes.",
-    "location": "S/C de Tenerife. España",
-    "email" : "Email",
-    "linkedin" : "Ver mi Linkedin",
-    "github" : "Accede a mi repositorio",
-    "car" : "Vehículo propio",
-    "proffesionalSkills" : "HABILIDADES PROFESIONALES",
-    "languages" : "IDIOMAS",
-    "english" : "Inglés",
-    "spanish" : "Español",
-    "otherKnowledge" : "OTROS CONOCIMIENTOS",
-    "repositoryDevelopment" : "Desarrollo colaborativo: Github y Bitbucket",
-    "testMethods" : "Métodos de pruebas: TDD e integración continua",
-    "testTools" : "Herramientas de pruebas: Selenium, Coveralls",
-    "manageTools" : "Gestión de proyectos: PivotalTracker, OpenProject",
-    "agileMthodology" : "Metodologías ágiles (Scrum)",
-    "clouds" : "Clouds: PaaS (Heroku, OpenShift)",
-    "database" : "SQL",
-    "os" : "SO Linux y Windows",
-    "coverLetterTitle" : "CARTA DE PRESENTACIÓN",
-    "coverLetter" : "Estimado Sr/Sra:"
-                    +"Mi nombre es Aarón Socas Gaspar, soy Graduado en Ingeniería Informática por la ULL. Decidí formarme en el ámbito tecnológico porque siempre me llamó la atención así como sus aplicaciones y salida laboral."
-                    +"Actualmente estoy trabajando para Atos dentro del departamento de I+D+i, investigando y desarrollando módulos relacionados con la seguridad y privacidad."
-                    +"Mi experiencia profesional como desarrollador la he realizado en la empresa INTEGRA Tecnología y Comunicación de Canarias, SL. También poseo conocimientos en administración de sistemas, tengo el título de formación profesional 2º grado de Diplomado Técnico Superior en Sistemas y estuve como becario en la Cámara de Comercio de Santa Cruz de Tenerife y en la Universidad de La Laguna. He orientado mi formación hacia la tecnología web, desarrollo en la nube, gestión de contenidos y seguridad."
-                    +"Soy una persona activa, curiosa. responsable, constante y perfeccionista, con una gran capacidad de trabajo y esfuerzo. Soy capaz de adaptarme a las distintas situaciones y a los cambios, pudiendo trabajar de forma efectiva en equipo o individual."
-                    +"Un cordial saludo,"
-                    +"Aarón Socas Gaspar"
+  "dropdownNav" : "Secciones",
+  "professionalExperienceNav" : "Experiencia profesional",
+  "educationNav" : "Educación",
+  "certificationsNav" : "Certificaciones",
+  "portfolioNav" : "Portafolio",
+  "professionalExperience" : "EXPERIENCIA PROFESIONAL",
+  "education" : "EDUCACIÓN",
+  "certifications" : "CERTIFICACIONES",
+  "portfolio" : "PORTAFOLIO",
+  "rol" : "Desarrollador",
+  "resume1": "Mi nombre es Aarón Socas Gaspar. Graduado en Ingeniería"
+            +"Informática por la Universidad de La Laguna. He orientado mi"
+            +"formación en el uso de tecnologías de la información, la cual"
+            +"engloba conocimientos relacionados con programación web y"
+            +"dispositivos móviles, seguridad y tratamiento de la información.",
+  "resume2" : "Me considero una persona trabajadora, activa y dinámica. Doy importancia al objetivo del proyecto y esto me hace esforzarme más en mi trabajo. Quiero seguir aprendiendo y colaborar en proyectos interesantes.",
+  "location": "S/C de Tenerife. España",
+  "email" : "Email",
+  "linkedin" : "Ver mi Linkedin",
+  "github" : "Accede a mi repositorio",
+  "car" : "Vehículo propio",
+  "proffesionalSkills" : "HABILIDADES PROFESIONALES",
+  "languages" : "IDIOMAS",
+  "english" : "Inglés",
+  "spanish" : "Español",
+  "otherKnowledge" : "OTROS CONOCIMIENTOS",
+  "repositoryDevelopment" : "Desarrollo colaborativo: Github y Bitbucket",
+  "testMethods" : "Métodos de pruebas: TDD e integración continua",
+  "testTools" : "Herramientas de pruebas: Selenium, Coveralls",
+  "manageTools" : "Gestión de proyectos: PivotalTracker, OpenProject",
+  "agileMthodology" : "Metodologías ágiles (Scrum)",
+  "clouds" : "Clouds: PaaS (Heroku, OpenShift)",
+  "database" : "SQL",
+  "os" : "SO Linux y Windows",
+  "coverLetterTitle" : "CARTA DE PRESENTACIÓN",
+  "coverLetter" : "Estimado Sr/Sra:"
+                  +"Mi nombre es Aarón Socas Gaspar, soy Graduado en Ingeniería Informática por la ULL. Decidí formarme en el ámbito tecnológico porque siempre me llamó la atención así como sus aplicaciones y salida laboral."
+                  +"Actualmente estoy trabajando para Atos dentro del departamento de I+D+i, investigando y desarrollando módulos relacionados con la seguridad y privacidad."
+                  +"Mi experiencia profesional como desarrollador la he realizado en la empresa INTEGRA Tecnología y Comunicación de Canarias, SL. También poseo conocimientos en administración de sistemas, tengo el título de formación profesional 2º grado de Diplomado Técnico Superior en Sistemas y estuve como becario en la Cámara de Comercio de Santa Cruz de Tenerife y en la Universidad de La Laguna. He orientado mi formación hacia la tecnología web, desarrollo en la nube, gestión de contenidos y seguridad."
+                  +"Soy una persona activa, curiosa. responsable, constante y perfeccionista, con una gran capacidad de trabajo y esfuerzo. Soy capaz de adaptarme a las distintas situaciones y a los cambios, pudiendo trabajar de forma efectiva en equipo o individual."
+                  +"Un cordial saludo,"
+                  +"Aarón Socas Gaspar"
 };
